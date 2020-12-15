0a4b011e9a3f5bd08b696e695ba88dcf1300a1d6
yjm6560 <yjm7558@nate.com>
2020-12-15 21:38:53 +0900

    Try to solve 1219

---
+++ 1219/1219.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	
+
+	return 0;
+}
