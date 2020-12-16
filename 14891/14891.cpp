3f651fa4259480ac87955ef5ce977be0ffa68a26
yjm6560 <yjm7558@nate.com>
2020-12-16 22:30:39 +0900

    Solve 14891

--- 14891/14891.cpp
+++ 14891/14891.cpp
@@ -15,9 +15,9 @@ int main()
 	cout.tie(0);
 	
 	int K, gear_idx, direction, answer = 0;
-	vector<vector<bool>> gear(N_GEAR, vector<bool>(N_WHEEL, false));
+	vector<vector<bool>> gear(N_GEAR+1, vector<bool>(N_WHEEL, false));
 	string input;
-	for(int i=0;i<N_GEAR;i++){
+	for(int i=1;i<=N_GEAR;i++){
 		cin >> input;
 		for(int j=0;j<N_WHEEL;j++){
 			gear[i][j] = input[j]=='0'? 0 : 1;
@@ -25,37 +25,40 @@ int main()
 	}
 
 	cin >> K;
+	vector<bool> diff(5, false);
+
 	while(K--){
 		cin >> gear_idx >> direction;
-		gear_idx--;
-		cout << gear_idx << " " << direction << endl;
-		rotate(gear[gear_idx].begin(), gear[gear_idx].begin() + (N_WHEEL - direction)%N_WHEEL, gear[gear_idx].end());
+		
+		fill(diff.begin(), diff.end(), false);
+	
 		int left_direction = -direction;
 		int right_direction = -direction;
-		for(int i=gear_idx-1;i>0;i--, left_direction *= -1){
-			if(gear[i+1][6] != gear[i][2]){
-				rotate(gear[i].begin(), gear[i].begin() + (N_WHEEL - left_direction)%N_WHEEL, gear[i].end());
-			} else {
-				break;
-			}
+
+		for(int i=gear_idx+1;i<=N_GEAR;i++){
+			//left check
+			diff[i] = gear[i][6] != gear[i-1][2];
 		}
-		
-		for(int i=gear_idx;i<N_GEAR-1;i++, right_direction *= -1){
-			cout << i << endl;
-			if(gear[i+1][6] != gear[i][2]){
-				rotate(gear[i+1].begin(), gear[i+1].begin() + (N_WHEEL - right_direction)%N_WHEEL, gear[i+1].end());
-			} else {
-				break;
-			}
+
+		for(int i=gear_idx-1;i>0;i--){
+			//right check
+			diff[i] = gear[i][2] != gear[i+1][6];
 		}
-		
-		for(int i=0;i<N_GEAR;i++){
-			for(auto x : gear[i])
-				cout << x;
-			cout << endl;
-		}
-		cout << endl;
+	
+		rotate(gear[gear_idx].begin(), (direction==1? gear[gear_idx].end() - 1 : gear[gear_idx].begin() + 1), gear[gear_idx].end());
+
+		for(int i=gear_idx+1;i<=N_GEAR && diff[i];i++, left_direction *= -1)
+			rotate(gear[i].begin(), (left_direction==1? gear[i].end() - 1 : gear[i].begin() + 1), gear[i].end());
+
+		for(int i=gear_idx-1;i>0 && diff[i];i--, right_direction *= -1)
+			rotate(gear[i].begin(), (right_direction==1? gear[i].end() - 1 : gear[i].begin() + 1), gear[i].end());
 	}
+	vector<int> squared = {0, 1, 2, 4, 8};
+	for(int i=1;i<=N_GEAR;i++){
+		if(gear[i][0])
+			answer += squared[i];
+	}
+	cout << answer << "\n";
 
 
 	return 0;
